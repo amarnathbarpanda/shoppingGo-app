c592752447bf17302138a620cfe66ea71f7a18c6
Amarnath Barpanda <47183341+amarnathbarpanda@users.noreply.github.com>
2022-09-11 17:09:21 +0530

    Products fetched from my-json-server

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 import './AllProducts.css';
-import {data} from '../utils/data';
 import CardItem from './CardItem';
 
 const AllProducts = () => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('https://my-json-server.typicode.com/amarnathbarpanda/ShoppingGo/products')
+        .then(res =>res.json())
+        .then(data => setProducts(data));
+    }, [products])
+    
+
     return (
         <div className="all__products">
-            <div className='sort__tag'><span>Sort by price</span></div>
+            <div className='sort__tag'>
+                <span>
+                    Sort by price
+                    <CancelIcon />
+                </span>
+            </div>
             <div className="card__container">
-                {data.map((product, index)=>(
+                {products?.map((product, index)=>(
                     <CardItem key={index} product={product}/>
                 ))}
             </div>
