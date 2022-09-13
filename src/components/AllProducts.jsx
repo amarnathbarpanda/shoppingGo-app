050f8382c8f1c3a3c56521222ccf6f5ec4c68d50
Amarnath Barpanda <47183341+amarnathbarpanda@users.noreply.github.com>
2022-09-13 18:05:43 +0530

    Add to cart button and sorting issue resolved

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -25,7 +25,6 @@ const AllProducts = () => {
         console.log(products);
     }, [productSelector]);
 
-    useEffect(()=>{ console.log("products updated");},[products, sortedData])
     
     const sortData = (a, b) => {
         if (a.price < b.price) {
@@ -37,12 +36,26 @@ const AllProducts = () => {
         return 0;
     }
 
+    const sortDataById = (a, b) => {
+        if (a.id < b.id) {
+            return -1;
+        }
+        if (a.id > b.id) {
+            return 1;
+        }
+        return 0;
+    }
+
     const onSort = () =>{
         if(!isSorted){
             const sortedProducts = products.sort(sortData);
-            setSortedData(sortedProducts);
-            setIsSorted(true);
+            setProducts(sortedProducts);
+        }else{
+            const sortedProducts = products.sort(sortDataById);
+            setProducts(sortedProducts);
         }
+
+        setIsSorted(!isSorted);
     }
 
     return (
@@ -50,14 +63,11 @@ const AllProducts = () => {
             <div className='sort__tag'>
                 <span onClick={()=> onSort()}>
                     Sort by price
-                    {isSorted && <CancelIcon onClick={()=> setIsSorted(false)} sx={{color: "gray", fontSize: '2rem'}}/>}
+                    {isSorted && <CancelIcon sx={{color: "gray", fontSize: '2rem'}}/>}
                 </span>
             </div>
             <div className="card__container">
-                {!isSorted && products.length && products?.map((product, index)=>(
-                    <CardItem key={index} product={product}/>
-                ))}
-                {isSorted && sortedData.length && sortedData?.map((product, index)=>(
+                { products.length && products?.map((product, index)=>(
                     <CardItem key={index} product={product}/>
                 ))}
             </div>
--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -2,43 +2,45 @@ import React from 'react';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Rating from '@mui/material/Rating';
+import { Button } from '@mui/material';
+import AddShoppingCartRoundedIcon from '@mui/icons-material/AddShoppingCartRounded';
 
 const CardItem = ({ product: { id, title, description, image, price, rating: { rate, count }, } }) => {
-  return (
-      <div className='card'>
-          <div className="product__image">
-              <img src={image} alt={title} />
-          </div>
-          <div className="product__info">
-              <div className="info__left">
-                  <div className="info__text">
-                      <span className='product__title'>{title}</span>
-                      <span className='product__price'>₹ {price}</span>
-                  </div>
+    return (
+        <div className='card'>
+            <div className="product__image">
+                <img src={image} alt={title} />
+            </div>
+            <div className="product__info">
+                <div className="info__left">
+                    <div className="info__text">
+                        <span className='product__title'>{title}</span>
+                        <span className='product__price'>₹ {price}</span>
+                    </div>
 
-                  <div className="ratings">
-                      <span>
-                          <Rating name="half-rating-read" defaultValue={rate} precision={0.5} readOnly />
-                      </span>
-                      <span className='rating__count'>({count})</span>
-                  </div>
-              </div>
-              <div className="info__right">
-                  <span className="product__desc">
-                      {description}
-                  </span>
-                  <div className="actions">
-                      <EditIcon sx={styles.edit} />
-                      <DeleteIcon sx={styles.delete} />
-                  </div>
-              </div>
-
-          </div>
-      </div>
-  )
+                    <div className="ratings">
+                        <span>
+                            <Rating name="half-rating-read" defaultValue={rate} precision={0.5} readOnly />
+                        </span>
+                        <span className='rating__count'>({count})</span>
+                    </div>
+                </div>
+                <div className="info__right">
+                    <span className="product__desc">
+                        {description}
+                    </span>
+                    <div className="actions">
+                        <Button startIcon={<AddShoppingCartRoundedIcon/>} variant="contained" color="success">Add to Cart</Button>
+                        <EditIcon sx={styles.edit} />
+                        <DeleteIcon sx={styles.delete} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-const styles={
+const styles = {
     edit: {
         fontSize: '2.3rem',
         color: '#1D3557',
